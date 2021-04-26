2dab07daa4c6bdf4e3d589f01d9fe92c405e449b
Daniel Hopson <hdhopson88@yahoo.com>
2021-04-25 19:07:30 -0700

    Updates controller to utilize stock service

--- aspnet/SJCRUDStock.WebApi/Controllers/StockController.cs
+++ aspnet/SJCRUDStock.WebApi/Controllers/StockController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SJCRUDStock.DataService.Interfaces;
+using SJCRUDStock.ObjectModel.Models;
 
 namespace SJCRUDStock.WebApi.Controllers
 {
@@ -12,13 +14,20 @@ namespace SJCRUDStock.WebApi.Controllers
     [Route("rest/stockjock/storage/[controller]")]
     public class StockController : ControllerBase
     {
+        private readonly IStockService stockService;
+
+        public StockController()
+        {
+            stockService = new StockService();
+        }
+    
         /// </summary>
         /// Allows access to stock symbol data
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Stock[]), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetSymbol() => Ok("Boom!");
+        public async Task<IActionResult> GetSymbol() => Ok(await stockService.GetStockList());
     }
 }
